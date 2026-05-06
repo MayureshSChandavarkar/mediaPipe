8b6d662cb31d9db05274b41d1297a7c5462b1a2e
MayureshSChandavarkar <pes1202203312@pesu.pes.edu>
2026-05-06 13:44:15 +0530

    minor changes

--- stream.py
+++ stream.py
@@ -6,28 +6,22 @@ app = Flask(__name__)
 @app.route('/stream')
 def stream():
     def generate():
-        # Using a 1x1 pixel preview off-screen as a workaround because --nopreview breaks the encoder on your Pi OS!
-        cmd = ["rpicam-vid", "--preview", "4000,4000,1,1", "-t", "0", "--codec", "mjpeg", "--width", "640", "--height", "480", "--framerate", "30", "--inline", "-o", "-"]
+        cmd = ["rpicam-vid", "-t", "0", "--codec", "mjpeg", "--width", "640", "--height", "480", "--framerate", "30", "--inline", "-o", "-"]
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         
-        try:
-            chunk = b''
-            while True:
-                data = process.stdout.read(4096)
-                if not data:
-                    break
-                chunk += data
-                a = chunk.find(b'\xff\xd8')
-                b = chunk.find(b'\xff\xd9')
-                if a != -1 and b != -1:
-                    jpg = chunk[a:b+2]
-                    chunk = chunk[b+2:]
-                    yield (b'--frame\r\n'
-                           b'Content-Type: image/jpeg\r\n\r\n' + jpg + b'\r\n')
-        finally:
-            # IMPORTANT: This ensures the camera is released when the stream stops!
-            process.terminate()
-            process.wait()
+        chunk = b''
+        while True:
+            data = process.stdout.read(4096)
+            if not data:
+                break
+            chunk += data
+            a = chunk.find(b'\xff\xd8')
+            b = chunk.find(b'\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = chunk[a:b+2]
+                chunk = chunk[b+2:]
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + jpg + b'\r\n')
 
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
