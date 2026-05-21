1e47e73ae36c044aaa644de3d47f64586ddd1899
MayureshSChandavarkar <pes1202203312@pesu.pes.edu>
2026-05-21 13:34:29 +0530

    edited the target ip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/pi-stream': {
-        target: 'http://192.168.0.102:8080',
+        target: 'http://10.20.201.30:8080', //kindly edit the ip and put ip of your target system here and make sure you are connected to the same internet
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/pi-stream/, '/stream'),
       }
